474a18c0cf70fb6d9cfedb513fcccd1f8fb10337
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-08-29 14:03:30 -0300

    Script R update

--- Scripts/RStudio/Script_2.R
+++ Scripts/RStudio/Script_2.R
@@ -1,8 +1,29 @@
 # Generate Correlation
-# require(graphics)
-resultado<- read.csv("C:/Users/felip/Documents/TCC2/Resultados_v2.csv", header = FALSE,
+# Gera Correlação dos navegadores da classe tradicional
+tradicional<- read.csv("C:/Users/felip/Documents/TCC2/Metricas_PorClasses/Resultados_Tradicional.csv", header = FALSE,
                      sep = ",")
 
-correlacao<-cor(resultado, method = c("spearman"))
+correlacaoTrad<-cor(tradicional, method = c("spearman"))
+round(correlacaoTrad, 2)
 
-correlacao
+write.csv(round(correlacaoTrad, 2), file = "C:/Users/felip/Documents/TCC2/Metricas_PorClasses/Corelacao_Tradicional.csv")
+
+
+# Gera Correlação dos navegadores da classe privacidade
+privacidade<- read.csv("C:/Users/felip/Documents/TCC2/Metricas_PorClasses/Resultados_Privacidade.csv", header = FALSE,
+                       sep = ",")
+
+correlacaoPri<-cor(privacidade, method = c("spearman"))
+round(correlacaoPri, 2)
+
+write.csv(round(correlacaoPri, 2), file = "C:/Users/felip/Documents/TCC2/Metricas_PorClasses/Corelacao_Privacidade.csv")
+
+# Gera Correlação dos navegadores da classe seguranca
+seguranca<- read.csv("C:/Users/felip/Documents/TCC2/Metricas_PorClasses/Resultados_Seguranca.csv", header = FALSE,
+                       sep = ",")
+correlacaoSec<-cor(seguranca, method = c("spearman"))
+round(correlacaoSec, 2)
+
+write.csv(round(correlacaoSec, 2), file = "C:/Users/felip/Documents/TCC2/Metricas_PorClasses/Corelacao_Seguranca.csv")
+
+
