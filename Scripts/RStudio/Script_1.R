d519b8b7afc27cb5a44c69ff88ef4975daa867ca
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-09-02 23:29:01 -0300

    update Script R

--- Scripts/RStudio/Script_1.R
+++ Scripts/RStudio/Script_1.R
@@ -1,13 +1,17 @@
 # Generate Empirical Cumulative Distribution Function
 require(graphics)
-resultado<- read.csv("C:/Users/felip/Documents/TCC2/Resultados.csv", header = FALSE,
+library(lattice)
+library(Hmisc)
+resultado<- read.csv("C:/Users/felip/Documents/TCC2/Scripts/RStudio/Resultados_Original.csv", head = TRUE,
                      sep = ",")
-resultado
 
-Fn <- ecdf(as.numeric(resultado[,1]))
-Fn
-Fn(as.numeric(resultado[,1]))
+columnpath <- c("C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/LOC_Média_por_pacote.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/L_Groovy.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/L_HTML.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/L_J.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/L_KT.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/L_XML.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/LOC.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/CBO.jpg", "C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/DIT.jpg",	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/LCOM.jpg",	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/NOC.jpg",	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/RFC.jpg",	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/WMC.jpg",	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/AHF.jpg", 	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/AIF.jpg", 	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/CF.jpg", 	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/MHF.jpg", 	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/MIF.jpg", 	"C:/Users/felip/Documents/TCC2/Scripts/RStudio/Graficos/PF.jpg")
+column <- c("LOC_Média_por_pacote", "L_Groovy", "L_HTML", "L_J", "L_KT", "L_XML", "LOC", "CBO", "DIT",	"LCOM",	"NOC",	"RFC",	"WMC",	"AHF", 	"AIF", 	"CF", 	"MHF", 	"MIF", 	"PF")
+options(digits=4)
+for(metric in 3:21){
+  resultado[,metric] <- as.numeric(sub(",",".",resultado[,metric],fixed=TRUE))
+  jpeg(filename = columnpath[metric-2], width = 350, height = 350, bg = "transparent")
+  Ecdf(resultado[,metric], group=resultado[,2], col=c('blue', 'red', 'green', 'orange'), xlab=column[metric-2],ylab="Quantidade")
+  dev.off()
+}
 
-plot(Fn, verticals = TRUE, col.points = "blue",
-     col.hor = "red", col.vert = "bisque")
-# lines(as.numeric(resultado[,1]), Fn(as.numeric(resultado[,1])), col = "blue")
