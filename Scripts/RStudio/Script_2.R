f096773df54afbd4d579f4d4ca4ee7c9df4c7f92
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-09-05 17:24:07 -0300

    update Script R

--- Scripts/RStudio/Script_2.R
+++ Scripts/RStudio/Script_2.R
@@ -1,23 +1,28 @@
-library("Hmisc")
-# Generate Correlation
 resultados<-read.csv("C:/Users/felip/Documents/TCC2/Resultados/Metricas_CSV/Resultados_Original.csv", header = FALSE,
                      sep = ",")
 
 #Calcula correlação das métricas dos navegadores da classe tradicional
 tradicionais<-subset(resultados, resultados[,21]==c("Tradicional"))
-correlacaoTradicionais<-rcorr(as.matrix(tradicionais[,3:20]))
-dfcorrelacaoTradicionais=data.frame(correlacaoTradicionais$P)
+for(metric in 3:20){
+  tradicionais[,metric] <- as.numeric(sub(",",".",tradicionais[,metric],fixed=TRUE))
+}
+dfcorrelacaoTradicionais<-cor(tradicionais[3:20],method = "spearman")
 
 #Calcula correlação das métricas dos navegadores da classe privacidade
 privacidade<-subset(resultados, resultados[,21]==c("Privacidade"))
-correlacaoPrivacidade<-rcorr(as.matrix(tradicionais[,3:20]))
-dfcorrelacaoPrivacidade=data.frame(correlacaoPrivacidade$P)
+for(metric in 3:20){
+  privacidade[,metric] <- as.numeric(sub(",",".",privacidade[,metric],fixed=TRUE))
+}
+dfcorrelacaoPrivacidade<-cor(privacidade[3:20],method = "spearman")
 
 #Calcula correlação das métricas dos navegadores da classe segurança
 seguranca<-subset(resultados, resultados[,21]==c("Seguranca"))
-correlacaoSeguranca<-rcorr(as.matrix(seguranca[,3:20]))
-dfcorrelacaoSeguranca=data.frame(correlacaoSeguranca$P)
+for(metric in 3:20){
+  seguranca[,metric] <- as.numeric(sub(",",".",seguranca[,metric],fixed=TRUE))
+}
+dfcorrelacaoSeguranca<-cor(seguranca[3:20],method = "spearman")
 
+#Salva os resuldados da corelação no arquivo csv
 xlsx::write.xlsx(dfcorrelacaoTradicionais,file='C:/Users/felip/Documents/TCC2/Resultados/Correlacao/Correlacao.xlsx',sheetName = 'correlacaoTradicionais')
 
 xlsx::write.xlsx(dfcorrelacaoPrivacidade,file='C:/Users/felip/Documents/TCC2/Resultados/Correlacao/Correlacao.xlsx',sheetName = 'correlacaoPrivacidade',append = T)
