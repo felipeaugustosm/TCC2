7d09f95b98f83ef39d8c63a73e6ba0c5cb72d507
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-09-09 10:00:15 -0300

    update Script

--- Scripts/RStudio/Script_1.R
+++ Scripts/RStudio/Script_1.R
@@ -1,10 +1,11 @@
-# Generate Empirical Cumulative Distribution Function
+# Gerar função de distribuição cumulativa empírica
 require(graphics)
 library(lattice)
 library(Hmisc)
 resultado<- read.csv("C:/Users/felip/Documents/TCC2/Resultados/Metricas_CSV/Resultados_Original.csv", head = TRUE,
                      sep = ",")
 
+#Salva os gráficos gerados das distribuição cumulativa
 columnpath <- c("C:/Users/felip/Documents/TCC2/Resultados/Graficos/LOC_Média_por_pacote.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/L_Groovy.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/L_HTML.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/L_J.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/L_KT.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/L_XML.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/LOC.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/CBO.jpg", "C:/Users/felip/Documents/TCC2/Resultados/Graficos/DIT.jpg",	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/LCOM.jpg",	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/NOC.jpg",	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/RFC.jpg",	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/WMC.jpg",	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/AHF.jpg", 	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/AIF.jpg", 	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/CF.jpg", 	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/MHF.jpg", 	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/MIF.jpg", 	"C:/Users/felip/Documents/TCC2/Resultados/Graficos/PF.jpg")
 column <- c("LOC_Média_por_pacote", "L_Groovy", "L_HTML", "L_J", "L_KT", "L_XML", "LOC", "CBO", "DIT",	"LCOM",	"NOC",	"RFC",	"WMC",	"AHF", 	"AIF", 	"CF", 	"MHF", 	"MIF", 	"PF")
 options(digits=4)
