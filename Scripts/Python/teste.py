70cac98d959492d1b5f2bc0101fe753a1d6fc4c4
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-09-09 22:31:35 -0300

    update Script

--- Scripts/Python/teste.py
+++ Scripts/Python/teste.py
@@ -28,7 +28,7 @@ frame = pd.DataFrame(pd.concat(resultados, ignore_index=False, sort=False))
 frame.fillna(0, inplace=True)
 
 # Substitui as virgulas por pontos nas colunas que contem resultados com formatação errada
-cols = ["LOC (Média por pacote)",	"L(Groovy)",	"L(HTML)",	"L(J)",	"L(KT)",	"L(XML)",	"LOC",	"AHF (%)",	"AIF (%)",	"CF (%)",	"MHF (%)",	"MIF (%)",	"PF (%)",	"CBO - Média",	"DIT – Média",	"LCOM – Média",	"NOC – Média",	"RFC – Média",	"WMC – Média",	"CBO",	"DIT",	"LCOM",	"NOC", "RFC", "WMC"]
+cols = ["LOC (Média por pacote)",	"L(Groovy)",	"L(HTML)",	"L(J)",	"L(KT)",	"L(XML)",	"LOC",	"AHF (%)",	"AIF (%)",	"CF (%)",	"MHF (%)",	"MIF (%)",	"PF (%)",	"CBO - Média",	"DIT – Média",	"LCOM – Média",	"NOC – Média",	"RFC – Média",	"WMC – Média"]
 for col in cols:  # Iterate over chosen columns
     frame[col] = (frame[col].replace(',','.', regex=True))
     frame[col] = frame[col].astype(float)
@@ -39,7 +39,7 @@ for col in cols:  # Iterate over chosen columns
 #Armazena os resultados em um arquivo csv
 os.chdir("C:/Users/felip/Documents/TCC2/Resultados/Metricas_CSV")
 
-resultadosFrame = pd.DataFrame(frame.groupby('Navegadores', as_index=False, sort=False)['LOC (Média por pacote)', 'L(Groovy)', 'L(HTML)', 'L(J)', 'L(KT)', 'L(XML)', 'LOC', 'AHF (%)', 'AIF (%)', 'CF (%)', 'MHF (%)', 'MIF (%)', 'PF (%)', 'CBO - Média', 'DIT – Média', 'LCOM – Média', 'NOC – Média', 'RFC – Média', 'WMC – Média', 'CBO', 'DIT', 'LCOM', 'NOC', 'RFC', 'WMC'].sum())
+resultadosFrame = pd.DataFrame(frame.groupby('Navegadores', as_index=False, sort=False)['LOC (Média por pacote)', 'L(Groovy)', 'L(HTML)', 'L(J)', 'L(KT)', 'L(XML)', 'LOC', 'AHF (%)', 'AIF (%)', 'CF (%)', 'MHF (%)', 'MIF (%)', 'PF (%)', 'CBO - Média', 'DIT – Média', 'LCOM – Média', 'NOC – Média', 'RFC – Média', 'WMC – Média'].sum())
 
 resultadosFrameBrowser = resultadosFrame.copy()
 
@@ -48,6 +48,5 @@ resultadosFrame['Classes'] = pd.Series(['Segurança', 'Tradicional', 'Privacidad
 resultadosFrameBrowser['Classes'] = pd.Series(['Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos', 'Todos'])
 
 resultadosFrame = resultadosFrame.append(resultadosFrameBrowser, ignore_index=True) 
-
 
 resultadosFrame.to_csv('Resultados_Original.csv', index=True)
