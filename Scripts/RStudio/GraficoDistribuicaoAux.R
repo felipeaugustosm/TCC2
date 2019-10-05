a873b0b5dc6016cd0242398a58311fc024612faf
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-10-05 20:53:21 -0300

    update texto

--- Scripts/RStudio/GraficoDistribuicaoAux.R
+++ Scripts/RStudio/GraficoDistribuicaoAux.R
@@ -1,3 +1,5 @@
+require(stringr)
+
 resultados<-read.csv("C:/Users/felip/Documents/TCC2/Resultados/Metricas_CSV/Resultados_Original.csv", header = FALSE,
                      sep = ",")
 
@@ -8,9 +10,11 @@ privacidade<-resultados[resultados[,22]%in%c("Privacidade"),]
 seguranca<-resultados[resultados[,22]%in%c("Seguranca"),]
 
 todos <-resultados[resultados[,22]%in%c("Todos"),]
-path="C:/Users/felip/Documents/TCC2/Resultados/Graficos/Distribuicao/Nova_pasta/"
+
+path="C:/Users/felip/Documents/TCC2/Resultados/Graficos/Distribuicao/"
 extensao=".jpeg"
-for(metric in 20:20){
+
+for(metric in 17:17){
   ditTradicionais.ecdf <- ecdf(as.numeric(as.character(tradicionais[,metric])))
   ditPrivacidade.ecdf <- ecdf(as.numeric(as.character(privacidade[,metric])))
   ditSeguranca.ecdf <- ecdf(as.numeric(as.character(seguranca[,metric])))
@@ -18,10 +22,10 @@ for(metric in 20:20){
   
   nameFile = paste(resultados[1,metric], extensao)
   pathnameFile = paste(path, nameFile)
-
+  pathnameFile <- str_trim(pathnameFile)
   jpeg(filename = pathnameFile, width = 1000, height = 1000)
     par(mar=c(5.1, 5.1, 4.1, 12.5), xpd=TRUE)
-    plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="Distribuição", xlab="Métrica RPC",las=1, main="", cex.axis = 2, cex.lab=2.5, xlim = c(0, 4))
+    plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="Distribuição", xlab="Métrica DIT",las=1, main="", cex.axis = 2, cex.lab=2.5, xlim = c(0, 4))
     lines(ditPrivacidade.ecdf, verticals=TRUE, do.points=FALSE, col="orange");
     lines(ditSeguranca.ecdf, verticals=TRUE, do.points=FALSE, col="blue")
     lines(ditSeguranca.ecdf, verticals=TRUE, do.points=FALSE, col="red")
