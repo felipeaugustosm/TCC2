62fd5a7fb1c33aa998adf712ee42a6dfe1395ac7
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-10-07 23:49:57 -0300

    update texto

--- Scripts/RStudio/GraficoDistribuicaoAux.R
+++ Scripts/RStudio/GraficoDistribuicaoAux.R
@@ -11,10 +11,10 @@ seguranca<-resultados[resultados[,22]%in%c("Seguranca"),]
 
 todos <-resultados[resultados[,22]%in%c("Todos"),]
 
-path="C:/Users/felip/Documents/TCC2/Resultados/Graficos/Distribuicao/"
+path="C:/Users/felip/Documents/TCC2/Resultados/Graficos/"
 extensao=".jpeg"
 
-for(metric in 17:17){
+metric<-21
   ditTradicionais.ecdf <- ecdf(as.numeric(as.character(tradicionais[,metric])))
   ditPrivacidade.ecdf <- ecdf(as.numeric(as.character(privacidade[,metric])))
   ditSeguranca.ecdf <- ecdf(as.numeric(as.character(seguranca[,metric])))
@@ -25,10 +25,9 @@ for(metric in 17:17){
   pathnameFile <- str_trim(pathnameFile)
   jpeg(filename = pathnameFile, width = 1000, height = 1000)
     par(mar=c(5.1, 5.1, 4.1, 12.5), xpd=TRUE)
-    plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="Distribuição", xlab="Métrica DIT",las=1, main="", cex.axis = 2, cex.lab=2.5, xlim = c(0, 4))
+    plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="Distribuição", xlab="Métrica RFC",las=1, main="", cex.axis = 2, cex.lab=2.5, xlim = c(0, 4))
     lines(ditPrivacidade.ecdf, verticals=TRUE, do.points=FALSE, col="orange");
     lines(ditSeguranca.ecdf, verticals=TRUE, do.points=FALSE, col="blue")
     lines(ditSeguranca.ecdf, verticals=TRUE, do.points=FALSE, col="red")
     legend("topright", inset=c(-0.23,0), legend=c("Tradicionais", "Privacidade", "Segurança", "Todos"), col=c("yellow", "orange", "blue", "red"), lty=1:2, cex=1.5)
   dev.off()
-}
