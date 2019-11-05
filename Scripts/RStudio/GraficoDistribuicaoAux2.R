5d3d12fbd1062484e1b403be88401fad99b81dba
Felipe Augusto Silva Marques <felipe.augustosm@gmail.com>
2019-11-05 00:44:59 -0300

    update

--- Scripts/RStudio/GraficoDistribuicaoAux2.R
+++ Scripts/RStudio/GraficoDistribuicaoAux2.R
@@ -9,15 +9,15 @@ seguranca<-resultados[resultados[,22]%in%c("Seguranca"),]
 
 todos <-resultados[resultados[,22]%in%c("Todos"),]
 
-metric<-8
+metric<-16
 par(mar=c(5.1, 5.1, 4.1, 7.5), xpd=TRUE)
 ditTradicionais.ecdf <- ecdf(as.numeric(as.character(tradicionais[,metric])))
 ditPrivacidade.ecdf <- ecdf(as.numeric(as.character(privacidade[,metric])))
 ditSeguranca.ecdf <- ecdf(as.numeric(as.character(seguranca[,metric])))
 ditTodos.ecdf <- ecdf(as.numeric(as.character(todos[,metric])))
-#plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="Distribuição", xlab="Métrica LOC HTML",las=1, main="", xlim = c(0, 5))
-plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="Distribuição", xlab="(f) Métrica LOC XML",las=1, main="", xlim = c(0, 300000))
-lines(ditPrivacidade.ecdf, verticals=TRUE, do.points=FALSE, col="orange");
+#plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="F(X)", xlab="Métrica LOC HTML",las=1, main="", xlim = c(0, 15000000))
+plot(ditTradicionais.ecdf, verticals=TRUE, do.points=FALSE, col="yellow", ylab="F(X)", xlab="CBO",las=1, main="", xlim = c(0, 15))
+lines(ditPrivacidade.ecdf, verticals=TRUE, do.points=FALSE, col="orange")
 lines(ditSeguranca.ecdf, verticals=TRUE, do.points=FALSE, col="blue")
 lines(ditTodos.ecdf, verticals=TRUE, do.points=FALSE, col="red")
 legend("topright", inset=c(-0.42,0), legend=c("Tradicionais", "Privacidade", "Segurança", "Todos"), col=c("yellow", "orange", "blue", "red"), lty=1:2, cex=0.8)
